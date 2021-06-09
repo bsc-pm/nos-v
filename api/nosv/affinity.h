5e2530c52036b45a163ce709eee41e95013fd44f
David Alvarez <david.alvarez@bsc.es>
2021-06-09 17:48:05 +0200

    Affinity API

--- api/nosv/affinity.h
+++ api/nosv/affinity.h
@@ -12,36 +12,41 @@
 #include <sched.h>
 #include <stdint.h>
 
-/* Fully generic affinity API */
-/* Use cpu_set_t for everything */
-
-struct nosv_affinity;
-typedef nosv_affinity *nosv_affinity_t;
-
 typedef enum nosv_affinity_type {
 	PREFERRED,
 	STRICT
 } nosv_affinity_type_t;
 
-/* Register a custom affinity mask */
-int nosv_affinity_register(nosv_affinity_t *affinity /* out */, const cpu_set_t *cpus, const nosv_affinity_type_t type);
-/* Make sure no tasks are using this affinity mask before unregistering it */
-int nosv_affinity_unregister(nosv_affinity_t affinity);
+typedef enum nosv_affinity_level {
+	CPU,
+	NUMA,
+	USER_COMPLEX
+} nosv_affinity_level_t;
+
+typedef uint32_t nosv_affinity_t;
+
+static inline nosv_affinity_t nosv_affinity_get(uint32_t index, nosv_affinity_level_t level, nosv_affinity_type_t type)
+{
+	return (level << 30) | (type << 29) | index;
+}
 
 /* For me it's unclear if we want to do per-task affinity or per task-type */
 nosv_affinity_t nosv_get_task_affinity(nosv_task_t task);
 void nosv_set_task_affinity(nosv_task_t task, nosv_affinity_t affinity);
 
+/* Example */
+/*
 static inline void example() {
 	nosv_affinity_t affinity;
 	nosv_task_t task;
 
 	// Bind to CPU 1
-	cpu_set_t cpus;
-	CPU_SET(1, &cpus);
-	nosv_affinity_register(&affinity, &cpus, STRICT);
+	affinity = nosv_affinity_get(1, CPU, STRICT);
+	nosv_set_task_affinity(task, affinity);
 
+	// Affinity to NUMA Node 0
+	affinity = nosv_affinity_get(0, NUMA, PREFERRED);
 	nosv_set_task_affinity(task, affinity);
 }
-
+*/
 #endif // NOSV_AFFINITY_H
