85ab052ac5dce4cb0aa72af936ad29cfa34c55bf
David Alvarez <david.alvarez@bsc.es>
2021-08-16 18:01:24 +0200

    Support out of tree builds

--- tests/wrapper.sh
+++ tests/wrapper.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+test_driver=$1
+shift
+
 tap_driver=$1
 shift
 
@@ -9,7 +12,7 @@ args=("$@")
 location=${#args[@]}
 
 program=${args[${location} - 1]}
-args[${location} - 1]="${current_dir}/driver"
+args[${location} - 1]="${test_driver}"
 args[${location}]=$program
 
 $tap_driver "${args[@]}"
