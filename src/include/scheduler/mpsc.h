fd22aea869a4aac5b7586cf230710085505bdc91
David Alvarez <david.alvarez@bsc.es>
2021-08-19 14:53:10 +0200

    CPU Batching

--- src/include/scheduler/mpsc.h
+++ src/include/scheduler/mpsc.h
@@ -17,9 +17,8 @@
 #include "generic/spinlock.h"
 #include "memory/slab.h"
 
-// TODO: Define a mode where there are multiple queues, but not one per cpu
-#define __MPSC_ONE_PER_CPU 1
-#define __MPSC_POPS_BEFORE_ROTATE 256
+// One queue per 4 CPUs
+#define __MSPC_CPU_BATCH 1
 
 /*
 	This is a queue that can accept multiple producers and a single consumer.
@@ -27,27 +26,33 @@
 	consumed fast enough, it can fill up (which is fine).
 */
 
+typedef struct mpsc_subqueue {
+	nosv_spinlock_t qspin __cacheline_aligned;
+	spsc_queue_t *queue;
+} mspc_subqueue_t;
+
 typedef struct mpsc_queue {
 	size_t nqueues;
 	size_t current;
-	spsc_queue_t **queues;
-	nosv_spinlock_t qspin __cacheline_aligned;
+	mspc_subqueue_t *queues;
 } mpsc_queue_t;
 
 static inline mpsc_queue_t *mpsc_alloc(size_t nqueues, size_t slots)
 {
 	mpsc_queue_t *queue = (mpsc_queue_t *)salloc(sizeof(mpsc_queue_t), -1);
 	assert(queue);
-	queue->queues = (spsc_queue_t **)salloc(sizeof(spsc_queue_t *) * (nqueues + 1), -1);
+	nqueues = (nqueues + __MSPC_CPU_BATCH - 1) / __MSPC_CPU_BATCH;
+	queue->queues = (mspc_subqueue_t *)salloc(sizeof(mspc_subqueue_t) * (nqueues + 1), -1);
 
 	assert(nqueues > 0);
 
-	for (int i = 0; i < nqueues + 1; ++i)
-		queue->queues[i] = spsc_alloc(slots);
+	for (int i = 0; i < nqueues + 1; ++i) {
+		queue->queues[i].queue = spsc_alloc(slots);
+		nosv_spin_init(&queue->queues[i].qspin);
+	}
 
 	queue->nqueues = nqueues;
 	queue->current = 0;
-	nosv_spin_init(&queue->qspin);
 
 	return queue;
 }
@@ -55,16 +60,18 @@ static inline mpsc_queue_t *mpsc_alloc(size_t nqueues, size_t slots)
 static inline int mpsc_push(mpsc_queue_t *queue, void *value, int cpu)
 {
 	assert(value);
-	int ret;
+	int q, ret;
 
 	if (unlikely(cpu < 0)) {
-		nosv_spin_lock(&queue->qspin);
-		ret = spsc_push(queue->queues[queue->nqueues], value);
-		nosv_spin_unlock(&queue->qspin);
+		q = queue->nqueues;
 	} else {
-		assert(cpu < queue->nqueues);
-		ret = spsc_push(queue->queues[cpu], value);
+		assert(cpu < queue->nqueues * __MSPC_CPU_BATCH);
+		q = cpu / __MSPC_CPU_BATCH;
 	}
+
+	nosv_spin_lock(&queue->queues[q].qspin);
+	ret = spsc_push(queue->queues[q].queue, value);
+	nosv_spin_unlock(&queue->queues[q].qspin);
 
 	return ret;
 }
@@ -80,7 +87,7 @@ static inline int mpsc_pop_batch(mpsc_queue_t *queue, void **value, int cnt)
 	int ret;
 
 	// First, try to pop from current
-	total = spsc_pop_batch(queue->queues[current], value, cnt);
+	total = spsc_pop_batch(queue->queues[current].queue, value, cnt);
 	if (total == cnt) {
 		// Rotate the queue
 		queue->current = (current + 1) % (nqueues + 1);
@@ -91,7 +98,7 @@ static inline int mpsc_pop_batch(mpsc_queue_t *queue, void **value, int cnt)
 	current = (current + 1) % (nqueues + 1);
 
 	while (current != start && total < cnt) {
-		ret = spsc_pop_batch(queue->queues[current], value, cnt - total);
+		ret = spsc_pop_batch(queue->queues[current].queue, value, cnt - total);
 		total += ret;
 		value += ret;
 
@@ -114,13 +121,13 @@ static inline int mpsc_pop(mpsc_queue_t *queue, void **value)
 	assert(current <= nqueues);
 
 	// First, try to pop from current
-	if (spsc_pop(queue->queues[current], value))
+	if (spsc_pop(queue->queues[current].queue, value))
 		return 1;
 
 	current = (current + 1) % (nqueues + 1);
 
 	while (current != start) {
-		if (spsc_pop(queue->queues[current], value)) {
+		if (spsc_pop(queue->queues[current].queue, value)) {
 			queue->current = current;
 			return 1;
 		}
