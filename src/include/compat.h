ebb0643f82b7c0c27b1a0f31e36d64ca085fbb83
Rodrigo Arias Mallo <rodrigo.arias@bsc.es>
2021-10-25 14:53:47 +0200

    Add copyright notice in missing headers

--- src/include/compat.h
+++ src/include/compat.h
@@ -1,3 +1,9 @@
+/*
+	This file is part of nOS-V and is licensed under the terms contained in the COPYING file.
+
+	Copyright (C) 2021 Barcelona Supercomputing Center (BSC)
+*/
+
 #ifndef COMPAT_H
 #define COMPAT_H
 
