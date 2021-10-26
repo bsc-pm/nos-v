c0c22535a8ab982720b40f80aed31bc0dc646a09
David Alvarez <david.alvarez@bsc.es>
2021-10-26 12:11:24 +0200

    Add test for external thread attach

---
+++ tests/basic/attach-external.c
@@ -0,0 +1,45 @@
+#include "test.h"
+
+#include <nosv.h>
+#include <pthread.h>
+#include <sched.h>
+
+static nosv_task_type_t type;
+static test_t test;
+
+
+static void *start_routine(void *arg)
+{
+	nosv_task_t task;
+
+	nosv_attach(&task, type, 0, NULL, NOSV_ATTACH_NONE);
+	test_ok(&test, "External thread can attach");
+
+	nosv_detach(NOSV_DETACH_NONE);
+	test_ok(&test, "External thread can detach");
+
+	return NULL;
+}
+
+int main()
+{
+	test_init(&test, 4);
+
+	nosv_init();
+
+	nosv_type_init(&type, NULL, NULL, NULL, "main", NULL, NOSV_TYPE_INIT_EXTERNAL);
+
+	pthread_t external_thread;
+	pthread_create(&external_thread, NULL, &start_routine, NULL);
+
+	nosv_task_t task;
+	nosv_attach(&task, type, 0, NULL, NOSV_ATTACH_NONE);
+	test_ok(&test, "Main thread can attach");
+	nosv_detach(NOSV_DETACH_NONE);
+	test_ok(&test, "Main thread can detach");
+
+	pthread_join(external_thread, NULL);
+
+	nosv_type_destroy(type, NOSV_DESTROY_NONE);
+	nosv_shutdown();
+}
