bsc-pm/nos-v

commit 5369818f85025a63ee83a8c6aaae77699bc7c165

Respect quantum in immediate successor chains

src/hardware/threads.c
@@ -219,11 +219,15 @@ static inline void worker_execute_or_delegate(nosv_task_t task, cpu_t *cpu, int 
 
 static inline void *worker_start_routine(void *arg)
 {
+	uint64_t timestamp;
+	int pid;
+
 	current_worker = (nosv_worker_t *)arg;
 	assert(current_worker);
 	assert(current_worker->cpu);
 	cpu_set_current(current_worker->cpu->logic_id);
 	current_worker->tid = gettid();
+	pid = current_worker->logic_pid;
 
 	// Initialize hardware counters for the thread
 	hwcounters_thread_initialize(current_worker);
@@ -244,7 +248,14 @@ static inline void *worker_start_routine(void *arg)
 		nosv_task_t task = current_worker->task;
 
 		if (!task && current_worker->immediate_successor) {
-			task = current_worker->immediate_successor;
+			// Check if our quantum is up, to prevent hoarding CPU resources
+			// This will cause some immediate successor chains to be broken, which
+			// could be optimized by using SCHED_GET_NONBLOCK first.
+			if (scheduler_should_yield(pid, cpu_get_current(), &timestamp))
+				scheduler_submit(current_worker->immediate_successor);
+			else
+				task = current_worker->immediate_successor;
+
 			worker_set_immediate(NULL);
 		}
 

commit 5ecbd222246ef36fbfd0b499104624021a52180a

sched: don't break IS chains
Prevent breaking immediate successor chains when there are no other
tasks pending from other processes.
This addresses a performance regression introduced by 5369818f.

src/hardware/threads.c
@@ -246,21 +246,37 @@ static inline void *worker_start_routine(void *arg)
 
 	while (!atomic_load_explicit(&threads_shutdown_signal, memory_order_relaxed)) {
 		nosv_task_t task = current_worker->task;
+		int cpu = cpu_get_current();
 
 		if (!task && current_worker->immediate_successor) {
-			// Check if our quantum is up, to prevent hoarding CPU resources
-			// This will cause some immediate successor chains to be broken, which
-			// could be optimized by using SCHED_GET_NONBLOCK first.
-			if (scheduler_should_yield(pid, cpu_get_current(), &timestamp))
-				scheduler_submit(current_worker->immediate_successor);
-			else
+			// Check if our quantum is up to prevent hoarding CPU resources. This is only
+			// necessary here as quantum is already taken into account in "scheduler_get"
+			if (scheduler_should_yield(pid, cpu, &timestamp)) {
+				// To prevent immediate successor chains to be unnecessarily broken if no tasks are up,
+				// try to obtain a new task first
+				// TODO: Ideally there should be a scheduler flag to say: only give me a task if it belongs to a different process
+				nosv_task_t candidate = scheduler_get(cpu, SCHED_GET_NONBLOCKING);
+
+				if (candidate->type->pid != pid) {
+					scheduler_submit(current_worker->immediate_successor);
+					task = candidate;
+				} else {
+					// Return the task because it is from our process
+					// in this case, we just want to keep executing the immediate successors instead
+					scheduler_submit(candidate);
+					task = current_worker->immediate_successor;
+					// Reset the quantum
+					scheduler_reset_accounting(pid, cpu);
+				}
+			} else {
 				task = current_worker->immediate_successor;
+			}
 
 			worker_set_immediate(NULL);
 		}
 
 		if (!task && current_worker->cpu)
-			task = scheduler_get(cpu_get_current(), SCHED_GET_DEFAULT);
+			task = scheduler_get(cpu, SCHED_GET_DEFAULT);
 
 		if (task) {
 			// We can only reach this point in two cases: