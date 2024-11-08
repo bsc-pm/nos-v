3f3b0e90a28fc584eb570c042140f669b69035f0
Aleix Boné <aleix.boneribo@bsc.es>
2024-11-08 11:47:00 +0100

    tests(cond): test pthread wait

--- tests/basic/cond.c
+++ tests/basic/cond.c
@@ -15,13 +15,14 @@
 #include "common/utils.h"
 
 
-#define NTASKS 8
+#define NTASKS 100
 #define TEST_TIMEOUT 4000
 
 enum cond_test_flag {
 	BROADCAST    = __ZEROBITS,
 	SIGNAL		 = __BIT(0), // 0 -> broadcast, 1 -> signal
 	NOWAIT		 = __BIT(1), // Don't wait for timed tasks to expire
+	PTHREAD		 = __BIT(2), // Use pthread_mutex instead of nosv_mutex
 };
 
 typedef enum cond_test_flag cond_test_flag_t;
@@ -38,30 +39,49 @@ int ready = 0;
 
 nosv_cond_t cond;
 nosv_mutex_t mutex;
+pthread_mutex_t pthread_mutex;
 
 struct metadata {
 	int tid;
 	struct timespec *deadline;
+	cond_test_flag_t flags;
 };
+
+static inline void lock(const cond_test_flag_t flags) {
+	if (flags & PTHREAD) CHECK(pthread_mutex_lock(&pthread_mutex));
+	else CHECK(nosv_mutex_lock(mutex));
+}
+
+static inline void unlock(const cond_test_flag_t flags) {
+	if (flags & PTHREAD) CHECK(pthread_mutex_unlock(&pthread_mutex));
+	else CHECK(nosv_mutex_unlock(mutex));
+}
 
 void task_run(nosv_task_t task)
 {
 	atomic_fetch_add_explicit(&track_init, 1, memory_order_relaxed);
-	CHECK(nosv_mutex_lock(mutex));
 	struct metadata *meta = nosv_get_task_metadata(task);
 	struct timespec *deadline = meta->deadline;
+	lock(meta->flags);
 	if (deadline) {
 		// If timedwait, we don't want to check for ready value, otherwise
 		// we cannot wait for the tasks to wake up on their own in the test.
-		CHECK(nosv_cond_timedwait(cond, mutex, deadline));
+		if (meta->flags & PTHREAD)
+			CHECK(nosv_cond_timedwait_pthread(cond, &pthread_mutex, deadline));
+		else
+			CHECK(nosv_cond_timedwait(cond, mutex, deadline));
+
 		ready--;
 	} else {
 		while (!ready) {
-			CHECK(nosv_cond_wait(cond, mutex));
+			if (meta->flags & PTHREAD)
+				CHECK(nosv_cond_wait_pthread(cond, &pthread_mutex));
+			else
+				CHECK(nosv_cond_wait(cond, mutex));
 		}
 		ready--;
 	}
-	CHECK(nosv_mutex_unlock(mutex));
+	unlock(meta->flags);
 }
 
 void task_comp(nosv_task_t task)
@@ -72,8 +92,10 @@ void task_comp(nosv_task_t task)
 	atomic_fetch_add_explicit(&completed[meta->tid], 1, memory_order_relaxed);
 }
 
-void test_cond(const char *msg, const cond_test_flag_t flags, const int timed_tasks, const int td)
+void test_cond(const char *pref, const char *label, const cond_test_flag_t flags, const int timed_tasks, const long time_ms)
 {
+	char msg[255];
+	sprintf(msg, "%s-%s", pref, label);
 	atomic_store_explicit(&track_init, 0, memory_order_relaxed);
 	atomic_store_explicit(&track_completed, 0, memory_order_relaxed);
 
@@ -83,22 +105,23 @@ void test_cond(const char *msg, const cond_test_flag_t flags, const int timed_ta
 	struct timespec deadline;
 	if (timed_tasks) {
 		clock_gettime(CLOCK_MONOTONIC, &deadline);
-		deadline.tv_nsec += td*1000000; // *1ms
+		deadline.tv_nsec += time_ms*1000000; // *1ms
 	}
 
 	for (int i = 0; i < NTASKS; i++) {
 		CHECK(nosv_create(&tasks[i], task_type, sizeof(struct metadata), NOSV_CREATE_NONE));
 		struct metadata *meta = nosv_get_task_metadata(tasks[i]);
 		meta->tid = i;
+		meta->flags = flags;
 
 		// Only use timedwait for the first n tasks
 		meta->deadline = i < timed_tasks ? &deadline : NULL;
 	}
 
 	// broadcast into empty queue, should do nothing
-	CHECK(nosv_mutex_lock(mutex));
+	lock(flags);
 	CHECK(nosv_cond_broadcast(cond));
-	CHECK(nosv_mutex_unlock(mutex));
+	unlock(flags);
 
 	// Submit all tasks. Each task will call nosv_cond_wait
 	for (int i = 0; i < NTASKS; i++)
@@ -125,10 +148,10 @@ void test_cond(const char *msg, const cond_test_flag_t flags, const int timed_ta
 		// At this point, all tasks should have blocked into the scheduler
 		for (int i = 1; i <= NTASKS; i++) {
 			// Unblock one task
-			CHECK(nosv_mutex_lock(mutex));
+			lock(flags);
 			ready++;
 			CHECK(nosv_cond_signal(cond));
-			CHECK(nosv_mutex_unlock(mutex));
+			unlock(flags);
 
 			// Wait for that task to finish
 			while (i > atomic_load_explicit(&track_completed, memory_order_relaxed))
@@ -142,11 +165,10 @@ void test_cond(const char *msg, const cond_test_flag_t flags, const int timed_ta
 			}
 		}
 	} else { // broadcast
-		CHECK(nosv_mutex_lock(mutex));
+		lock(flags);
 		ready += NTASKS;
 		CHECK(nosv_cond_broadcast(cond));
-		CHECK(nosv_mutex_unlock(mutex));
-
+		unlock(flags);
 	}
 
 	while (NTASKS != atomic_load_explicit(&track_completed, memory_order_relaxed))
@@ -168,62 +190,61 @@ void init_objs() {
 	// Prepare the global cond and mutex
 	CHECK(nosv_cond_init(&cond, NOSV_COND_NONE));
 	CHECK(nosv_mutex_init(&mutex, NOSV_MUTEX_NONE));
+	CHECK(pthread_mutex_init(&pthread_mutex, NULL));
 }
 
 void free_objs() {
 	// Free the cond object and mutex
 	CHECK(nosv_cond_destroy(cond));
 	CHECK(nosv_mutex_destroy(mutex));
+	CHECK(pthread_mutex_destroy(&pthread_mutex));
 }
+
+#define NVARIANTS 4
 
 int main()
 {
 	nosv_task_t task;
 
-	test_init(&test, 24);
+	test_init(&test, 2*7*NVARIANTS);
 
 	// Init nosv
 	CHECK(nosv_init());
 	CHECK(nosv_attach(&task, NULL, "main", NOSV_ATTACH_NONE));
 	CHECK(nosv_type_init(&task_type, task_run, NULL, task_comp, "task", NULL, NULL, NOSV_TYPE_INIT_NONE));
 
-	init_objs();
+	cond_test_flag_t variants[NVARIANTS] = {BROADCAST, SIGNAL, BROADCAST | PTHREAD, SIGNAL | PTHREAD};
+	char *labels[NVARIANTS] = {"broadcast", "signal", "broadcast-pthread", "signal-pthread"};
 
-	// Run the first test
-	test_cond("broadcast", BROADCAST, 0, 0);
-	// Repeat, this time reusing the cond
-	test_cond("broadcast-reuse", BROADCAST, 0, 0);
+	const int timeout_ms = 250;
 
-	// All tasks with timedwait 1 second in future
-	test_cond("broadcast-timed-full", BROADCAST, NTASKS, 200);
-	// Half the tasks with timedwait, 1 second in future
-	test_cond("broadcast-timed-half", BROADCAST, NTASKS/2, 200);
-	// All tasks with timedwait, with expired deadline (100 seconds ago)
-	test_cond("broadcast-timed-expired", BROADCAST, NTASKS, -1000);
+	for (int i = 0; i < NVARIANTS; ++i) {
+		init_objs();
 
-	// Half the tasks with timedwait, 1 second in future
-	test_cond("broadcast-timed-full-nowait", BROADCAST | NOWAIT, NTASKS, 200);
+		const cond_test_flag_t flags = variants[i];
+		const char *label = labels[i];
 
-	// Reset objects for signal test
-	free_objs();
-	init_objs();
+		// Run the first test
+		test_cond(label, "basic", flags, 0, 0);
+		// Repeat, this time reusing the cond
+		test_cond(label, "reuse", flags, 0, 0);
 
-	// Run signal test
-	test_cond("signal", SIGNAL, 0, 0);
-	// Repeat, this time reusing the cond
-	test_cond("signal-reuse", SIGNAL, 0, 0);
+		// All tasks with timed
+		test_cond(label, "timed-full", flags, NTASKS, timeout_ms);
+		// Half the tasks timed
+		test_cond(label, "timed-half", flags, NTASKS/2, timeout_ms);
+		// All tasks timed, with expired deadline (100 seconds ago)
+		test_cond(label, "timed-expired", flags, NTASKS, -100000);
 
-	// Signal timedwaits
-	test_cond("signal-timed-full", SIGNAL, NTASKS, 200);
-	// Half the tasks with timedwait, 1 second in future
-	test_cond("signal-timed-half", SIGNAL, NTASKS/2, 200);
-	// All tasks with timedwait, with expired deadline (100 seconds ago)
-	test_cond("signal-timed-expired", SIGNAL, NTASKS, -1000);
+		// Half the tasks timed, but we don't wait for them
+		test_cond(label, "timed-half-nowait", flags | NOWAIT, NTASKS/2, timeout_ms);
+		// All the tasks timed, but we don't wait for them
+		test_cond(label, "timed-full-nowait", flags | NOWAIT, NTASKS, timeout_ms);
 
-	test_cond("signal-timed-full-nowait", SIGNAL | NOWAIT, NTASKS, 200);
+		// Reset objects for signal test
+		free_objs();
+	}
 
-	// Shutdown nosv
-	free_objs();
 	CHECK(nosv_detach(NOSV_DETACH_NONE));
 	CHECK(nosv_shutdown());
 }
