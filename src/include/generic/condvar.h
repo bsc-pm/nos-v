fe569048145bc7b654efd860ac55efb6b832a8ca
Aleix Roca Nonell <aleix.rocanonell@bsc.es>
2026-02-25 15:53:57 +0100

    Use a semaphore instead of condvars in condvar.h
    The current nosv_condvar_t uses conditional varibles which uses two
    futexes (mutex and condvar) and the "signaled" flag. Instead, this patch
    uses a semaphore (single futex).
    
    It is worth noting that the assert check on the "signaled" flag cannot
    be replicated, but I have added a raced check that might help
    nevertheless.

--- src/include/generic/condvar.h
+++ src/include/generic/condvar.h
@@ -8,72 +8,50 @@
 #define CONDVAR_H
 
 #include <assert.h>
-#include <pthread.h>
+#include <errno.h>
+#include <semaphore.h>
 
 #include "compiler.h"
 
 typedef struct condition_var {
-	pthread_mutex_t mutex;
-	pthread_cond_t condvar;
-	int signaled;
+	sem_t sem;
 } nosv_condvar_t;
 
 static inline void nosv_condvar_init(nosv_condvar_t *condvar)
 {
-	pthread_mutexattr_t mattr;
-	pthread_condattr_t cattr;
-	__maybe_unused int res;
+	int res;
 
 	assert(condvar);
-
-	res = pthread_mutexattr_init(&mattr);
+	res = sem_init(&condvar->sem, 1, 0);
 	assert(!res);
-	res = pthread_condattr_init(&cattr);
-	assert(!res);
-
-	res = pthread_mutexattr_setpshared(&mattr, PTHREAD_PROCESS_SHARED);
-	assert(!res);
-	res = pthread_condattr_setpshared(&cattr, PTHREAD_PROCESS_SHARED);
-	assert(!res);
-
-	res = pthread_mutex_init(&condvar->mutex, &mattr);
-	assert(!res);
-	res = pthread_cond_init(&condvar->condvar, &cattr);
-	assert(!res);
-
-	pthread_mutexattr_destroy(&mattr);
-	pthread_condattr_destroy(&cattr);
-
-	condvar->signaled = 0;
 }
 
 static inline void nosv_condvar_destroy(nosv_condvar_t *condvar)
 {
+	int res;
+
 	assert(condvar);
-	pthread_mutex_destroy(&condvar->mutex);
-	pthread_cond_destroy(&condvar->condvar);
+	res = sem_destroy(&condvar->sem);
+	assert(!res);
 }
 
 static inline void nosv_condvar_wait(nosv_condvar_t *condvar)
 {
+	int res;
+
 	assert(condvar);
-
-	pthread_mutex_lock(&condvar->mutex);
-	while (!condvar->signaled)
-		pthread_cond_wait(&condvar->condvar, &condvar->mutex);
-
-	condvar->signaled = 0;
-	pthread_mutex_unlock(&condvar->mutex);
+	while ((res = sem_wait(&condvar->sem)) && errno == EINTR) {}
+	assert(!res);
 }
 
 static inline void nosv_condvar_signal(nosv_condvar_t *condvar)
 {
-	pthread_mutex_lock(&condvar->mutex);
-	assert(!condvar->signaled);
-	condvar->signaled = 1;
-	pthread_mutex_unlock(&condvar->mutex);
+	int res;
 
-	pthread_cond_signal(&condvar->condvar);
+	assert(condvar);
+	assert(sem_getvalue(&condvar->sem, &res) || res == 0);
+	res = sem_post(&condvar->sem);
+	assert(!res);
 }
 
 #endif // CONDVAR_H
